dd4e40625715ae3a3d58041915262054b43f24e9
Manuel Montenegro <manuel@nucypher.com>
2022-07-05 18:51:49 +0200

    Use new NPM module for global ongoing rewards calc
    A new NPM module for ongoing rewards calculation will allow to
    modularize the code. The React component `Ongoing Rewards Stats` has
    been migrated to this new NPM package.

--- src/components/OngoingRewardsStats/OngoingRewardsStats.jsx
+++ src/components/OngoingRewardsStats/OngoingRewardsStats.jsx
@@ -1,31 +1,32 @@
 import React from "react"
-import { useQuery, gql } from "@apollo/client"
-import { OngoingRewardsStatsCalc } from "./OngoingRewardsStatsCalc"
+import { Fragment } from "react"
+import { useState, useEffect } from "react"
+import BigNumber from "bignumber.js"
+import { getTotalOngoingRewards } from "@manumonti/staking-rewards-calculation"
 
 export function OngoingRewardsStats({ timestamp }) {
-  // TODO: Max amount of items you can get in a query is 100.
-  // adding 'first: 1000' is a WA to get more than 100 stakes,
-  // but the most correct option is to use GraphQL pagination.
-  const ONGOING_STAKES_QUERY = gql`
-    query OngoingStakesJuneFirst($timestamp: String) {
-      epoches(
-        first: 1000
-        orderBy: timestamp
-        where: { timestamp_gte: $timestamp }
-      ) {
-        id
-        timestamp
-        duration
-        totalAmount
-      }
+  const decimals = new BigNumber(1e18)
+  const [reward, setReward] = useState(null)
+
+  const getReward = async () => {
+    setReward(await getTotalOngoingRewards(timestamp))
+  }
+
+  useEffect(() => {
+    if (timestamp && !reward) {
+      getReward()
     }
-  `
-
-  const { loading, error, data } = useQuery(ONGOING_STAKES_QUERY, {
-    variables: { timestamp },
   })
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>Error {error.message}</div>
 
-  return <OngoingRewardsStatsCalc queryData={data} startTimestamp={timestamp} />
+  if (!reward) return <div>Loading...</div>
+
+  return (
+    <Fragment>
+      <div>Ongoing Rewards generated since June 1st:</div>
+      <div>
+        <strong>{reward.div(decimals).toFixed(0)}</strong>
+      </div>
+      <small>in Ether units (10^18)</small>
+    </Fragment>
+  )
 }
